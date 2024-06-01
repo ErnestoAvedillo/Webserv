e7e6e3cd10d9ddcad888886c9c59176cf3500ef0
eavedill <eavedillo@yahoo.es>
2024-06-01 13:51:27 +0200

    add web for test prouposes

---
+++ www/web4/cgi/test.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+const string ENV[24] = {
+	"COMSPEC", "DOCUMENT_ROOT", "GATEWAY_INTERFACE",
+	"HTTP_ACCEPT", "HTTP_ACCEPT_ENCODING",
+	"HTTP_ACCEPT_LANGUAGE", "HTTP_CONNECTION",
+	"HTTP_HOST", "HTTP_USER_AGENT", "PATH",
+	"QUERY_STRING", "REMOTE_ADDR", "REMOTE_PORT",
+	"REQUEST_METHOD", "REQUEST_URI", "SCRIPT_FILENAME",
+	"SCRIPT_NAME", "SERVER_ADDR", "SERVER_ADMIN",
+	"SERVER_NAME", "SERVER_PORT", "SERVER_PROTOCOL",
+	"SERVER_SIGNATURE", "SERVER_SOFTWARE"};
+
+int main(int av, char **ac)
+{
+	(void)av;
+	cout << "<html>\n";
+	cout << "<head>\n";
+	cout << "<title>CGI Environment Variables</title>\n";
+	cout << "</head>\n";
+	cout << "<body>\n";
+	cout << "<table border = \"0\" cellspacing = \"2\">\n";
+
+	cout << "<tr><td>" << "Argumentos de entrada." << "</td></tr>\n";
+
+	for (int i = 1; i < av; i++)
+	{
+		cout << "<tr><td>" << "Argumento de entrada Nº" << i << "= " << ac[i] << "</td></tr>\n";
+	}
+	for (int i = 0; i < 24; i++)
+	{
+		cout << "<tr><td>" << ENV[i] << "</td><td>";
+
+		// attempt to retrieve value of environment variable
+		char *value = getenv(ENV[i].c_str());
+		if (value != 0)
+		{
+			cout << value;
+		}
+		else
+		{
+			cout << "Environment variable does not exist.";
+		}
+		cout << "</td></tr>\n";
+	}
+
+	cout << "</table><\n";
+	cout << "</body>\n";
+	cout << "</html>\n";
+
+	return 0;
+}
