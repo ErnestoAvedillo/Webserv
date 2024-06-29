d61826262bfd2c8beee91fd6b5b248ae1511deca
Joseph Cheel nakashima <jcheel-n@car3s2.42barcelona.com>
2024-06-29 17:19:44 +0200

    CGI constructor with environment

--- mandatory/inc/CGI.hpp
+++ mandatory/inc/CGI.hpp
@@ -20,7 +20,7 @@ class CGI : public Environment
 		std::string CGIFolder;
 		std::string fileName;
 		std::string fileArgs;
-		char **env;
+		// char **env;
 		bool isCGI;
 		std::vector <ExtendedString> args;
 		std::map<std::string, std::string> CGIExtensions;
